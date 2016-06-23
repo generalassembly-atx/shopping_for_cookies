0e1dfc31b1341b0df5d00c4e87c716f80fb8cf54
Christian Mansour <c.mansour@miami.edu>
2016-06-22 19:40:12 -0500

    shopping-list

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,18 @@ var Product = require('../models/product');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { products: /* ??? */ });
+  res.render('index', { products: Product.all });
 });
 
 router.post('/cart', function(req, res, next) {
   // How do we add an item to the cart?
+  var cookieObject = parseInt(req.body.id);
+  var product = Product.find(cookieObject);
+  console.log(product);
+  req.session.cart.push(product);
+  // session.product = response;
+  res.redirect('/');
+
 });
 
 router.delete('/cart', function(req, res, next) {
