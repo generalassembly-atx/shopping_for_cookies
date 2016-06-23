34627aed756ae1befe9c0e1fbd0cd58b18c79324
Susannah Waite <susannahwaite2@gmail.com>
2016-06-23 08:23:20 -0500

    adds cookies to shopping cart

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,16 @@ var Product = require('../models/product');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { products: /* ??? */ });
+  res.render('index', { products: Product.all });
 });
 
 router.post('/cart', function(req, res, next) {
-  // How do we add an item to the cart?
+  var temp = parseInt(req.body.id);
+  var product = Product.find(temp);
+  // console.log(product);
+  req.session.cart.push(product);
+  console.log(req.session.cart);
+  res.redirect('/');
 });
 
 router.delete('/cart', function(req, res, next) {
